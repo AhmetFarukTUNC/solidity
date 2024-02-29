1aaf26534b2fd7bc66f19979dab0180093146dc8
YAZILIMCIABII <atunc8402@gmail.com>
2024-02-29 23:33:22 +0300

    We added new version of inbox folder

---
+++ inbox/test/Inbox.test.js
@@ -0,0 +1,67 @@
+const assert = require("assert");
+
+const ganache = require("ganache-cli");
+
+const Web3 = require("web3");
+
+const web3 = new Web3(ganache.provider());
+
+const {interface,bytecode} = require("../compile"); 
+
+let accounts;
+
+let inbox;
+
+beforeEach(async ()=>{
+    // web3.eth.getAccounts()
+    // .then(fetchedAccounts => {
+    //     console.log(fetchedAccounts);
+    // });
+
+    accounts = await web3.eth.getAccounts();
+
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data:bytecode, 
+    arguments: ['Hi,There!']})
+    .send({from:accounts[0],gas:"1000000"});
+
+})
+
+describe("Inbox",()=>{
+    it("deploys a contract", () => {
+        assert.ok(inbox.options.address);
+
+        
+    });
+
+    it("has a default message", async () => {
+        
+    })
+})
+
+// class Car{
+//     park(){
+//         return "stopped";
+//     }
+
+//     drive(){
+//         return "vroom";
+//     }
+
+// }
+
+// let car;
+
+// beforeEach(() => {
+//     console.log("Car class created.");
+//     car = new Car();
+// })
+
+// describe("Car", () => {
+//     it("can park", () => {
+//         assert.equal(car.park(), "stopped")
+//     }),
+//     it("can drive", () => {
+//         assert.equal(car.drive(), "vroom")
+//     }) 
+// })
