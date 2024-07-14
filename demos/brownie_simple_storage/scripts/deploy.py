23c38c4b12775178b681995c769de06c5b8f851e
YAZILIMCIABII <atunc8402@gmail.com>
2024-07-15 00:28:56 +0300

    We updated solidity repo

--- demos/brownie_simple_storage/scripts/deploy.py
+++ demos/brownie_simple_storage/scripts/deploy.py
@@ -1,10 +1,52 @@
 
-from brownie import accounts,config
+import os
+from brownie import accounts,config,SimpleStorage,network
 
 def deploy_simple_storage():
+    # Way 1
+    # account = accounts.load("account1")
+    # print(account)
+
+    # Way 2
+    # account = accounts.add(os.getenv("PRIVATE_KEY"))
+    # print(account)
     
-    account = accounts.add(config["wallets"]["from_key"])
-    print(account)
+
+    # Way 3
+    # account = accounts.add(config["wallets"]["from_key"])
+    # print(account)
+
+    # Way 4
+
+    # account = accounts[0]
+
+    # print(account)
+
+    account = get_account()
+
+    simple_storage = SimpleStorage.deploy({"from":account})
+
+    print(simple_storage)
+
+    stored_value = simple_storage.retreive()
+
+    print(stored_value)
+
+    transaction = simple_storage.store(15,{"from":account})
+
+    print(transaction)
+
+    transaction.wait(1)
+
+    updated_stored_value = simple_storage.retreive()
+
+    print(updated_stored_value)
+
+def get_account():
+    if network.show_active() == "devolopment":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
 
 def main():
     deploy_simple_storage()
